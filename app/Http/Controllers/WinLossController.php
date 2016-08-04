16382fad44612bfa5d22bc49643bb1d6137cd078
Mike <mikedph@gmail.com>
2016-08-04 19:15:46 +0800

    Added response to storing excel file

--- app/Http/Controllers/WinLossController.php
+++ app/Http/Controllers/WinLossController.php
@@ -9,59 +9,63 @@ use BackDoor\file as file;
 
 class WinLossController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    //
-  }
-
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function create()
-  {
-    //
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
-    $uploadDir = '/var/www/BackDoor/public/upload/excel_files/';
-    $uploadFile = $uploadDir . basename($_FILES['file']['name']);
-    $tempPath = $_FILES['file']['tmp_name'];
-    $fileAttribute = [
-      'name' => $_FILES['file']['name'],
-      'type' => $_FILES['file']['type'],
-      'size' => $_FILES['file']['size'],
-      'real_path' => $uploadDir
-    ];
-
-    if (move_uploaded_file($tempPath, $uploadFile)) {
-
-      $answer = array('answer' => 'File transfer completed');
-      $json = json_encode($answer);
-
-      file::insert($fileAttribute);
-
-      return $json;
-
-    } else {
-
-      return 'No files';
+    /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        //
     }
 
-  }
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function create()
+    {
+        //
+    }
+
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request $request
+    * @return \Illuminate\Http\Response
+    */
+    public function store(Request $request)
+    {
+
+        $uploadDir = public_path().'/upload/excel_files/';
+        $uploadFile = $uploadDir . basename($_FILES['file']['name']);
+        $tempPath = $_FILES['file']['tmp_name'];
+
+
+        if (move_uploaded_file($tempPath, $uploadFile))
+        {
+            $data = new File;
+
+            $data->name = $_FILES['file']['name'];
+            $data->type = $_FILES['file']['type'];
+            $data->size = $_FILES['file']['size'];
+            $data->real_path = $uploadDir;
+
+            if($data->save()) {
+                return response()->json(array(
+                    'success' => true,
+                    'content' => $data
+                ));
+            }
+
+        }
+        else
+        {
+            return response()->json(['success' => false]);
+        }
+
+    }
 
   /**
    * Display the specified resource.
