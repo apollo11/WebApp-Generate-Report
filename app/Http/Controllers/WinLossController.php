15245f3c18a71011f6dfe3cfa9459ce0bd3faeb6
apollomm <amalapote@gmail.com>
2016-08-05 17:26:29 +0800

    Remove added space

--- app/Http/Controllers/WinLossController.php
+++ app/Http/Controllers/WinLossController.php
@@ -9,90 +9,89 @@ use BackDoor\file as file;
 
 class WinLossController extends Controller
 {
-    /**
-    * Display a listing of the resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    //
+  }
 
-    /**
-    * Show the form for creating a new resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-    * Store a newly created resource in storage.
-    *
-    * @param  \Illuminate\Http\Request $request
-    * @return \Illuminate\Http\Response
-    */
-    public function store(Request $request)
-    {
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
 
-      $uploadDir = public_path().'/upload/excel_files/';
-      $tempPath = $_FILES['file']['tmp_name'];
-      $fileName = $_FILES['file']['name'];
-      $fileBasename = substr($fileName, 0, strripos($fileName,'.')); // get file extension;
-      $fileExt = substr($fileName, strripos($fileName, '.')); // get file name
-      $fileSize = $_FILES['file']['size'];
-      $allowed_file_types = array('.xlsx', '.xls');
+    $uploadDir = public_path() . '/upload/excel_files/';
+    $tempPath = $_FILES['file']['tmp_name'];
+    $fileName = $_FILES['file']['name'];
+    $fileBasename = substr($fileName, 0, strripos($fileName, '.')); // get file extension;
+    $fileExt = substr($fileName, strripos($fileName, '.')); // get file name
+    $fileSize = $_FILES['file']['size'];
+    $allowed_file_types = array('.xlsx', '.xls');
 
 
-      if (in_array($fileExt, $allowed_file_types) && ($fileSize < 200000)) {
+    if (in_array($fileExt, $allowed_file_types) && ($fileSize < 200000)) {
+
+      /**
+       * Rename file
+       */
+      $renameFileName = md5($fileBasename) . $fileExt;
+
+      if (file_exists($uploadDir . $renameFileName)) {
+
+        return response()->json(['error' => 'file already exist']);
+
+      } else {
 
         /**
-         * Rename file
+         * Move uploaded file
          */
-        $renameFileName = md5($fileBasename). $fileExt;
+        move_uploaded_file($tempPath, $uploadDir . $renameFileName);
 
-        if(file_exists($uploadDir . $renameFileName)) {
+        /**
+         * Save file data
+         */
+        $data = new File;
+        $data->name = $renameFileName;
+        $data->type = $_FILES['file']['type'];
+        $data->size = $fileSize;
+        $data->real_path = $uploadDir . $renameFileName;
 
-          return response()->json(['error'=> 'file already exist']);
+        if ($data->save()) {
 
-        }else{
+          return $this->createJsonResponse(true, $data);
 
-          /**
-           * Move uploaded file
-           */
-          move_uploaded_file($tempPath, $uploadDir.$renameFileName);
+        } else {
 
-          /**
-           * Save file data
-           */
-          $data = new File;
-          $data->name = $renameFileName;
-          $data->type = $_FILES['file']['type'];
-          $data->size = $fileSize;
-          $data->real_path = $uploadDir.$renameFileName;
-
-          if($data->save()) {
-
-            return $this->createJsonResponse(true, $data);
-
-          }else{
-
-            return $this->createJsonResponse(true);
-
-          }
+          return $this->createJsonResponse(true);
         }
-      }elseif ($fileSize > 200000) {
-
-        return response()->json(['error'=>'Entity too large']);
-
-      }else{
-
-        return response()->json(['error'=> implode(', ',$allowed_file_types)]);
       }
+    } elseif ($fileSize > 200000) {
+
+      return response()->json(['error' => 'Entity too large']);
+
+    } else {
+
+      return response()->json(['error' => implode(', ', $allowed_file_types)]);
     }
+  }
 
   /**
    * Display the specified resource.
@@ -103,13 +102,13 @@ class WinLossController extends Controller
   public function show($id = null)
   {
 
-      if (null != $id) {
-          $files = File::find($id);
-      } else {
-          $files =  File::all();
-      }
+    if (null != $id) {
+      $files = File::find($id);
+    } else {
+      $files = File::all();
+    }
 
-      return $this->createJsonResponse(true, $files);
+    return $this->createJsonResponse(true, $files);
   }
 
   /**
