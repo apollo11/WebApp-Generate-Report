1207427662a03c95569997209f8752ef5c67f5e4
Mike <mikedph@gmail.com>
2016-08-15 14:20:40 +0800

    Added API for getting winloss,games and player data by filename

--- app/WinLoss.php
+++ app/WinLoss.php
@@ -14,19 +14,36 @@ class WinLoss extends Model
     }
 
 
-    public function getData($aArrFields, $aIntId = null)
+    private function getData($aArrFields, $aArrQuery = array())
     {
         $winLoss = DB::table('win_losses')->select($aArrFields);
 
         // find by id if id exists
-        if (null != $aIntId) {
-            $winLoss = $winLoss->where('id','=', $aIntId);
+        if ($aArrQuery) {
+            $winLoss = $winLoss->where($aArrQuery);
         }
 
         // get data
         $winLossData = $winLoss->get();
-
         return $winLossData;
+    }
+
+    public function getDataById($aArrFields, $aIntId = null)
+    {
+        $arrQuery = array();
+
+        if (null != $aIntId)
+        {
+            $arrQuery = array('id' => $aIntId);
+        }
+
+        return $this->getData($aArrFields, $arrQuery);
+    }
+
+    public function getDataByFileName($aArrFields, $aStrFilename)
+    {
+        $arrQuery = array('file_name' => $aStrFilename);
+        return $this->getData($aArrFields, $arrQuery);
     }
 
 
